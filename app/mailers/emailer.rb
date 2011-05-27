f7f74d3039f1018bfcaf3ccc73797b73de1d2244
Roberto Breve <rbreve@gmail.com>
2011-05-27 18:48:12 -0400

    emailer

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,12 +1,3 @@
 class Emailer < ActionMailer::Base
-  def contact(recipient, subject, message, sent_at = Time.now, header)
-       @subject = subject
-       @recipients = recipient
-       @from = 'no-reply@yourdomain.com'
-       @sent_on = sent_at
-       @body["title"] = 'Request Received'
-      @body["email"] = 'sender@yourdomain.com'
-      @body["message"] = message
-       @headers["X-SMTPAPI"] = header
-    end
+  default :from => "from@example.com"
 end
