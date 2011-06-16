0708d03e493b0db768ad0dc4a7176bc5d5dca1b7
Roberto Breve <rbreve@gmail.com>
2011-06-16 03:07:58 -0400

    auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]  
-    #print auth.to_yaml
+    print auth.to_yaml
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     
     session[:user_id] = user.id
@@ -25,6 +25,6 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Signout Out"
+    redirect_to root_url, :notice => "Signed Out"
   end
 end
